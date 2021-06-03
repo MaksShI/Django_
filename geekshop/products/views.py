ad95f2da1ccfb59a23e7d5a02eac74ba7fd48bd9
MaksShI <76102358+MaksShI@users.noreply.github.com>
2021-06-03 10:52:45 +0300

    lessons2

--- geekshop/products/views.py
+++ geekshop/products/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+        'title': 'магазин',
+
+    }
+    return render(request, 'products/index.html', context)
 
 def products(request):
-    return render(request, 'products/products.html')
+    context = {
+        'title': 'продукты',
+        'price': '2585.9'
+    }
+    return render(request, 'products/products.html',context)
+
+
+
+
